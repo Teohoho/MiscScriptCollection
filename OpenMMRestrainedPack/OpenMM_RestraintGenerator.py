2fe051a023cde0b79e29b111316717dc2d42ccfb
teohoho <teo.sulea@gmail.com>
2021-03-29 13:59:07 +0300

    Added a periodic flag to harmonic. Also notes

--- OpenMMRestrainedPack/OpenMM_RestraintGenerator.py
+++ OpenMMRestrainedPack/OpenMM_RestraintGenerator.py
@@ -4,17 +4,14 @@ from simtk.unit import *
 import sys
 
 class GenerateRestraint:
-    """
+	
+	"""
     A class that makes it easy to generate a wide array
     of restraining force objects, given an OpenMM simulation
     object
 
     Parameters
     ----------
-
-    OpenMMSim:  OpenMMSimulationObject
-                OpenMM Simulation Object where we get the particle
-                positions from.
 
     HarmonicIndices:     str
                 Path to a file containing atom indices that will be used
@@ -23,42 +20,64 @@ class GenerateRestraint:
     FlatbottomIndices:     str
                 Path to a file containing atom indices that will be later
                 used in the following force definitions.
-    """
-    def __init__(self, OpenMMSim, HarmonicIndices, FlatbottomIndices):
-        OpenMMSim = self.OpenMMSim
+	"""
+	def __init__(self, HarmonicIndices=None, FlatbottomIndices=None):
 
-        ## Read HarmonicIndices file, and generate a list of indices.
-        HarmonicList  = open(args.RestrainedCoMAtomsIn).readlines().split("\n")
-	HarmonicList = [x for x in HarmonicList if x]  ##Remove empty elements 
+		if (HarmonicIndices is not None):
+	      ## Read HarmonicIndices file, and generate a list of indices.
+			HarmonicList  = open(HarmonicIndices).read().split("\n")
+			HarmonicList = [x for x in HarmonicList if x]  ##Remove empty elements 
+	
+			for RestraintIx in range(len(HarmonicList)):
+				HarmonicList[RestraintIx] = HarmonicList[RestraintIx].split()   ##Split into n groups
+				HarmonicList[RestraintIx] = [int(x) for x in HarmonicList[RestraintIx] if x]  ##Turn to ints
+	
+			for Ix in HarmonicList:
+				print ("Harmonic restraint containing {} atoms detected.".format(len(Ix)))
+	            
+			self.HarmonicList = HarmonicList
 
-	for RestraintIx in range(len(HarmonicList)):
-	    HarmonicList[RestraintIx] = HarmonicList[RestraintIx].split()   ##Split into n groups
-	    HarmonicList[RestraintIx] = [int(x) for x in HarmonicList[RestraintIx] if x]  ##Turn to ints
+		if (FlatbottomIndices is not None):
+			FlatbottomList  = open(FlatbottomIndices).readlines().split("\n")
+			FlatbottomList = [x for x in FlatbottomList if x]  ##Remove empty elements 
+	
+			for RestraintIx in range(len(FlatbottomList)):
+				FlatbottomList[RestraintIx] = FlatbottomList[RestraintIx].split()   ##Split into n groups
+				FlatbottomList[RestraintIx] = [int(x) for x in FlatbottomList[RestraintIx] if x]  ##Turn to ints
+		
+			self.FlatbottomList = FlatbottomList
+	
+			if (len(FlatbottomList) != 2):
+				print ("Warning! The Flatbottom restraint file needs to have exactly 2 lines. (got {} lines)!".format(len(FlatbottomList)))
+				sys.exit()
 
-        for Ix in HarmonicList:
-            print ("Harmonic restraint containing {} atoms detected.".format(len(Ix)))
-            
+		if (HarmonicIndices is None) and (FlatbottomIndices is None):
+			print("No restraints given. Exitting...")
+			sys.exit()
 
-        FlatbottomList  = open(args.RestrainedCoMAtomsIn).readlines().split("\n")
-	FlatbottomList = [x for x in FlatbottomList if x]  ##Remove empty elements 
 
-	for RestraintIx in range(len(FlatbottomList)):
-	    FlatbottomList[RestraintIx] = FlatbottomList[RestraintIx].split()   ##Split into n groups
-	    FlatbottomList[RestraintIx] = [int(x) for x in FlatbottomList[RestraintIx] if x]  ##Turn to ints
-
-        if (len(FlatbottomList) != 2):
-            print ("Warning! The Flatbottom restraint file needs to have exactly 2 lines. (got {} lines)!".format(len(FlatbottomList)))
-            sys.exit()
-
-    def HarmonicRestraint(self, ParticleIx, K=10, dummy=False):
-        """
+	def HarmonicRestraint(self, OpenMMSim, ParticleIx, Periodic=False, K=10, dummy=False):
+	
+		"""
         Generate a Harmonic, atom position-based restraint.
 
         Parameters
         ----------
+    
+		  OpenMMSim:  		OpenMMSimulationObject
+                			OpenMM Simulation Object where we get the particle
+                			positions from.
 
         ParticleIx:     list of Ints
                         Indices of particles affected by the harmonic restraint.
+
+		  Periodic:			bool
+								Whether or not to consider the periodic positions of the particles
+								or not. For some reason, when minimizing a Lipid-Protein-Water system 
+								using OpenMM, it will be necesarry for this to be True if restraining the lipids.
+								If restraining only the protein, or other particles that are not at the edge of the
+								unitcell, you must turn this to False, otherwise the protein will violently leave
+								the water box, in an explosive display.
 
         K:              float
                         Value of the spring constant for the harmonic restraint. Given in 
@@ -75,36 +94,50 @@ class GenerateRestraint:
 
         HarmonicForceObject:    CustomExternalForce Object
                                 harmonic potential force object
-        """
+
+		  Notes
+		  -----
+
+		  We use OpenMMSim here instead of in the init in order to get the *current* position
+		  of all particles involved.
+
+		"""
 
         ## Define the equation for the Harmonic Force
-        CustomHarmonicForce = CustomExternalForce("springConstant * (periodicdistance(x, y, z, x0, y0, z0)^2)")
-        CustomHarmonicForce.addGlobalParameter("springConstant", k*kilocalories_per_mole/nanometer**2)
-        CustomHarmonicForce.addPerParticleParameter("x0")
-        CustomHarmonicForce.addPerParticleParameter("y0")
-        CustomHarmonicForce.addPerParticleParameter("z0")
+		#CustomHarmonicForce = CustomExternalForce("springConstant * (periodicdistance(x, y, z, x0, y0, z0)^2)")
+		CustomHarmonicForce = CustomExternalForce("springConstant*((x-x0)^2+(y-y0)^2+(z-z0)^2)")
+		CustomHarmonicForce.addGlobalParameter("springConstant", K*kilocalories_per_mole/nanometer**2)
+		CustomHarmonicForce.addPerParticleParameter("x0")
+		CustomHarmonicForce.addPerParticleParameter("y0")
+		CustomHarmonicForce.addPerParticleParameter("z0")
 
-        if (dummy==False):
-        ## Get positions from the OpenMMSim
-            Positions = self.OpenMMSim.context.getState(getPositions=True).getPositions()
-    
-            ## Iterate through the given ParticleIx and add the
+		if (dummy==False):
+		## Get positions from the OpenMMSim
+			Positions = OpenMMSim.context.getState(getPositions=True, enforcePeriodicBox=Periodic).getPositions()
+			#Positions = OpenMMSim.context.getState(getPositions=True, enforcePeriodicBox=True).getPositions()
+			print(Positions[0])    
+			print(ParticleIx[0])
+    			## Iterate through the given ParticleIx and add the
             ## proper atoms to the force object
-            for PartIx in ParticleIx:
-                PartX = Positions[PartIx][0].value_in_unit(nanometer)
-                PartY = Positions[PartIx][1].value_in_unit(nanometer)
-                PartZ = Positions[PartIx][2].value_in_unit(nanometer)
+			for PartIx in ParticleIx:
+				PartX = Positions[PartIx][0].value_in_unit(nanometer)
+				PartY = Positions[PartIx][1].value_in_unit(nanometer)
+				PartZ = Positions[PartIx][2].value_in_unit(nanometer)
                 
-                CustomHarmonicForce.addParticle(PartIx, [PartX, PartY, PartZ])
+				CustomHarmonicForce.addParticle(PartIx, [PartX, PartY, PartZ])
     
+		##DEBUG##
+		
+
+
             ## Print some info about the object
-            print ("A new harmonic force has been created:\nIs periodic: {}\nNumber of Particles: {}".format(CustomHarmonicForce.usesPeriodicBoundaryConditions(), CustomHarmonicForce.getNumParticles()))
+			print ("A new harmonic force has been created:\nIs periodic: {}\nNumber of Particles: {}".format(CustomHarmonicForce.usesPeriodicBoundaryConditions(), CustomHarmonicForce.getNumParticles()))
     
-        return CustomHarmonicForce
+		return CustomHarmonicForce
 
-    def FlatBottomRestraint(Group1,Group2, springConstant, tol, dummy=False):
+	def FlatBottomRestraint(Group1,Group2, springConstant, tol, dummy=False):
 
-        """A function that generates an OpenMM Force object, that applies
+		"""A function that generates an OpenMM Force object, that applies
         an external force between the centers of groups of particles.
         This particular implementation was written for 2 groups, and with
         a flatbottom restraint in mind, but can be easily modified.
@@ -146,19 +179,17 @@ class GenerateRestraint:
         """
         
         ## Define the equation for the flatbottom potential
-	CustomCoMForce = CustomCentroidBondForce(2, "(((abs(abs(distance(g1,g2)) - tol) + (abs(distance(g1,g2)) - tol))/2)^2) * k")
-	P1 = CustomCoMForce.addGlobalParameter("k", (100)*kilocalories_per_mole/nanometer**2)
-	P2 = CustomCoMForce.addGlobalParameter("tol", 2*nanometer)
+		CustomCoMForce = CustomCentroidBondForce(2, "(((abs(abs(distance(g1,g2)) - tol) + (abs(distance(g1,g2)) - tol))/2)^2) * k")
+		P1 = CustomCoMForce.addGlobalParameter("k", (100)*kilocalories_per_mole/nanometer**2)
+		P2 = CustomCoMForce.addGlobalParameter("tol", 2*nanometer)
 
-        if (dummy == False):
-
-            ## Define the two groups
-            G1 = CustomCoMForce.addGroup(Group1)
-            G2 = CustomCoMForce.addGroup(Group2)
+		if (dummy == False):
+		## Define the two groups
+			G1 = CustomCoMForce.addGroup(Group1)
+			G2 = CustomCoMForce.addGroup(Group2)
             
-            CustomCoMForce.addBond((G1,G2))
-        
-            print ("A new Center of Mass restraint was generated, containing {} atoms.".format(len(Group1) + len(Group2)))
+			CustomCoMForce.addBond((G1,G2))
+			print ("A new Center of Mass restraint was generated, containing {} atoms.".format(len(Group1) + len(Group2)))
 
-	return CustomCoMForce 
+		return CustomCoMForce 
 
